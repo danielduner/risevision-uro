bb8293a819058cfdbe10370f3dc5c21cf1277da6
Daniel Dunér <daniel.duner@gmail.com>
2014-08-26 02:25:06 +0200

    Use padding instead of nbsp

--- EventsV2/Events.js
+++ EventsV2/Events.js
@@ -369,7 +369,7 @@ RiseVision.Events.EventWidget.prototype.initDays = function() {
 	    $day.text("idag - " + currentDay.toString("dddd - d MMMM"));
 	}
 	else {
-	    $day.text(currentDay.toString("dddd - d MMMM") + "&nbsp;&nbsp;&nbsp;&nbsp;");
+	    $day.text(currentDay.toString("dddd - d MMMM") + "<span class='timeSpace'></span>");
 	}
 	
 	currentDay.setDate(currentDay.getDate() + 1);
